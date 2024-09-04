23d90f3e51296b22ed973026155fd7ff5b6ec482
T2Ag <titoobguia3@gmail.com>
2024-09-04 09:02:30 +0800

    first commit

---
+++ src/constants/common.ts
@@ -0,0 +1,16 @@
+export const NAME = 'Tito Agustin L. Obguia';
+
+// ABOUT PAGE
+export const ABOUT_TEXT_1 =
+  'As a 4th-year IT student fluent in Laravel, I am passionate about writing, testing, debugging, and maintaining software code to create functional applications and systems. With 4 years of experience, I possess expertise in programming languages, algorithms, data structures, and software development methodologies.';
+export const ABOUT_TEXT_2 =
+  'Currently in my 4th year as an IT student, I am responsible for developing and maintaining web applications from the frontend to the backend. I work closely with cross-functional teams to design, implement, and deploy scalable solutions that meet business objectives, leveraging my 4 years of experience and fluency in Laravel.';
+export const ABOUT_TEXT_3 =
+  'A versatile 4th-year IT student proficient in both frontend and backend development, capable of building complete web applications from start to finish. With 4 years of experience and strong understanding of various programming languages, frameworks, and databases, I am fluent in Laravel and committed to delivering high-quality solutions.';
+
+// RESUME PAGE
+export const RESUME_TEXT_1 =
+  'IT student fluent in Laravel with 4 years of experience building websites and web applications. Adept in modern JavaScript tools/frameworks, with a solid understanding of both front-end and back-end technologies and database management.';
+export const RESUME_TEXT_2 =
+  '4th-year IT student fluent in Laravel, with 4 years of experience in building websites and web applications using modern JavaScript tools/frameworks.';
+
